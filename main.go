0829dd6bb298792754cbcb7e346b1948ecf8eb41
Christoforos Charalambous <chrischaralambous14@gmail.com>
2021-08-23 23:22:21 +0300

    Removed Discord token Props to Discord auto detect

--- main.go
+++ main.go
@@ -2,37 +2,7 @@ package main
 
 import "fmt"
 
-var (
-	Token string = "ODc5MDQxNzUyODk4NjE3Mzc0.YSJ9bQ.zIcEt8M0SFP32lbkep_j7vqHifk"
-)
-
 func main() {
-	/*dg, err := discordgo.New("Bot " + Token)
-	if err != nil {
-		log.Fatal("error creating Discord session,", err)
-		return
-	}
-
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
-
-	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
-
-	// Open a websocket connection to Discord and begin listening.
-	err = dg.Open()
-	if err != nil {
-		log.Fatal("error opening connection,", err)
-		return
-	}
-
-	log.Println("Bot is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
-
-	dg.Close()*/
-
 	fmt.Println("Welcome to Rummy!")
 	initializeGame()
 	startGame()
